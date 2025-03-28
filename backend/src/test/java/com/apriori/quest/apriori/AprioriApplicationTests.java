a5ae3269ad1f6e25b5db30d8288aa4149ada85c1
bzsol <bereczzsolt2000@gmail.com>
2025-03-28 22:17:18 +0100

    initial commit

---
+++ backend/src/test/java/com/apriori/quest/apriori/AprioriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apriori.quest.apriori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprioriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
